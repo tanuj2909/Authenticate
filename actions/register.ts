56bed5cf863dbd9c2ed974fdf230b0c4586377e9
Tanuj <tanujnayal2909@gmail.com>
2024-01-14 00:22:33 +0530

    Added db and pisma

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,35 @@
 "use server";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
-
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async(values: z.infer<typeof RegisterSchema>) => {
+
     const validatedFields = RegisterSchema.safeParse(values);
 
     if(!validatedFields.success) {
         return { error: "Invalid fields!" };
     }
 
-    return { success: "Email sent!" };
+    const { name, email, password } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await getUserByEmail(email);
+
+    if(user) {
+        return { error: "Email already in use!" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    })
+
+
+    return { success: "User Created!" };
 }
