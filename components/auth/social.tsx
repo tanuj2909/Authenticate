8d48bfccdb7bd0422533c59d02a68fe092b9173e
Tanuj <tanujnayal2909@gmail.com>
2024-01-15 01:32:13 +0530

    callbacks and oAuth

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,22 +1,31 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+
+const onClick = ( provider: "google" | "github" ) => {
+    signIn(provider, {
+        callbackUrl: DEFAULT_LOGIN_REDIRECT
+    })
+}
+
     return <div className="flex justify-center items-center w-full gap-x-4">
         <Button
             size={"lg"}
             variant={"outline"}
-            onClick={() => {}}
+            onClick={() => onClick("google")}
         >
             <FcGoogle className="h-5 w-5"/>
         </Button>
         <Button
             size={"lg"}
             variant={"outline"}
-            onClick={() => {}}
+            onClick={() => onClick("github")}
         >
             <FaGithub className="h-5 w-5"/>
         </Button>
